d06dabc066d5db2293e4bf66633698cb9fec7cfa
msalekmouad <mouadmsalek@outlook.com>
2020-08-24 19:25:49 +0100

    Added questions and asnwers buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/question.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //Question index
+    int qIndex = 0;
+
+    //Hard coding Quizz questions
+    Question q1 = Question(
+        question: "Whats the biggest planet on solar system ?",
+        answers: ["Sun", "Jupiter", "Earth"],
+        rightAnswer: "Sun");
+    Question q2 = Question(
+        question: "Whats the language spoken in Germany",
+        answers: ["Russian", "Arabic", "Dutch"],
+        rightAnswer: "Dutch");
+    Question q3 = Question(
+        question: "Whats the population of Earth ?",
+        answers: ["10 Billion", "7.8 Billion", "5 Billion"],
+        rightAnswer: "7.8 Billion");
+
+    //Questions List
+    var questions = [q1, q2, q3];
+
+    //Answer pressed function
+
+    void ProcessAnswer(Question q, String a){
+      if(q.rightAnswer == a){
+        print("You got it right");
+        qIndex++;
+      }
+      else{
+        print("Wrong answer");
+      }
+    }
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quizz App'),
+          elevation: 0,
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        ),
+        body: Column(
+          children: [
+            Center(
+                child: Padding(
+                  child: Text(
+                    questions[0].question,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(50),
+            )),
+            RaisedButton(
+              child: Text(questions[0].answers[0]),
+              onPressed: ()=>{
+                ProcessAnswer(questions[0], questions[0].answers[0])
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            RaisedButton(
+              child: Text(questions[0].answers[1]),
+              onPressed: ()=>{
+                ProcessAnswer(questions[0], questions[0].answers[1])
+              },
+            ),
+            RaisedButton(
+              child: Text(questions[0].answers[2]),
+              onPressed: ()=>{
+                ProcessAnswer(questions[0], questions[0].answers[2])
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/question.dart
@@ -0,0 +1,7 @@
+class Question{
+  String question;
+  var answers;
+  String rightAnswer;
+
+  Question({this.question, this.answers, this.rightAnswer});
+}
